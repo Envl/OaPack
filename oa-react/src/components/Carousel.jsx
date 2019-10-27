3694823fb204ffc8769e828f98228ca615d813a7
GnimOay <yyaomingm@gmail.com>
2019-10-27 17:45:49 +0100

    fix logic bug

--- oa-react/src/components/Carousel.jsx
+++ oa-react/src/components/Carousel.jsx
@@ -10,39 +10,45 @@ function styleCalc(currentIndex, length) {
 const Carousel = props => {
   const [currentIndex, setIndex] = useState(0)
   // const contents = [0, 1, 2, 3]
-  const contents = props.children
-  const [styleList, setStyleList] = useState(styleCalc(0, contents.length))
+  const contents =
+    typeof props.children == 'object' ? [props.children] : props.children
+  console.log(contents, 'sss', [].push(props.children), props.children)
+  const [styleList, setStyleList] = useState(
+    styleCalc(0, contents && contents.length),
+  )
 
   return (
     <div
       {...props}
       className={`oa-carousel ${props.className ? props.className : ''}`}>
       <div className='carousel-indicators'>
-        {contents.map((item, index) => (
-          <div
-            className={`carousel-indicator indicator-${index} ${
-              index == currentIndex ? 'current-indi' : ''
-            }`}
-            key={`carousel-indicator-${index}`}
-            onMouseEnter={evt => {
-              const i = evt.target.className.match('indicator-(.* )')[1]
-              setIndex(i)
-              console.log('new i', i)
-              const tmp = styleCalc(i, contents.length)
-              setStyleList(tmp)
-            }}></div>
-        ))}
+        {contents &&
+          contents.map((item, index) => (
+            <div
+              className={`carousel-indicator indicator-${index} ${
+                index == currentIndex ? 'current-indi' : ''
+              }`}
+              key={`carousel-indicator-${index}`}
+              onMouseEnter={evt => {
+                const i = evt.target.className.match('indicator-(.* )')[1]
+                setIndex(i)
+                console.log('new i', i)
+                const tmp = styleCalc(i, contents.length)
+                setStyleList(tmp)
+              }}></div>
+          ))}
       </div>
-      {contents.map((item, index) => (
-        <div
-          className={`carousel-item item-${index}`}
-          key={`carousel-item-${index}`}
-          style={{
-            transform: styleList[index],
-          }}>
-          {item}
-        </div>
-      ))}
+      {contents &&
+        contents.map((item, index) => (
+          <div
+            className={`carousel-item item-${index}`}
+            key={`carousel-item-${index}`}
+            style={{
+              transform: styleList[index],
+            }}>
+            {item}
+          </div>
+        ))}
     </div>
   )
 }
