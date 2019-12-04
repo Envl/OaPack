69d7a5284e4fec668bf1aacdd069b3f9164d4520
GnimOay <yyaomingm@gmail.com>
2019-12-04 21:26:00 +0100

    sidebar autohide on mobile

--- oa-react/src/components/Sidebar.jsx
+++ oa-react/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 
 export function SidebarItem({brief, detail, to, className, ...props}) {
@@ -33,9 +33,36 @@ export function SidebarItem({brief, detail, to, className, ...props}) {
 }
 
 export function SidebarInjector({children, items, ...props}) {
+  const [hideBar, setHideBar] = useState(false)
+  let isScrollDown = false
+  let lastY = 0
+  let lastFlag = true
+  useEffect(() => {
+    function scrollHandler(params) {
+      isScrollDown = window.pageYOffset > lastY
+      lastY = window.pageYOffset
+      if (isScrollDown !== lastFlag) {
+        //update state
+        lastFlag = isScrollDown
+        console.log(isScrollDown)
+        setHideBar(isScrollDown)
+      }
+    }
+    // scroll detect
+    console.log(window.innerWidth, 'sds')
+    if (window.innerWidth < 768) {
+      window.addEventListener('scroll', scrollHandler)
+    }
+    return () => {
+      window.removeEventListener('scroll', scrollHandler)
+    }
+  }, [])
   return (
     <div className='element-with-sidebar'>
-      <div className={`oa-sidebar ${props.className ? props.className : ''}`}>
+      <div
+        className={`oa-sidebar ${props.className ? props.className : ''} ${
+          hideBar ? 'thin-bar' : ''
+        }`}>
         {items}
       </div>
       <div className='sidebar-placeholder'></div>
